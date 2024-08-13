069ad33285bd3ac9bf55d17dec38294ff39e8337
Jo Wagner <jowagner@limago.de>
2024-08-13 09:54:16 +0200

    initial commit

--- Tag2_01Observer/src/tiere/PigTooFatListener.java
+++ Tag2_01Observer/src/tiere/PigTooFatListener.java
@@ -4,3 +4,12 @@ public interface PigTooFatListener {
     /* normalerweise mit Eventklasse als Parameter */
     void pigTooFat(Schwein schwein);
 }
+/*
+class event {
+    Object source
+    String propertyName
+    Object oldValue
+    Object newValue
+}
+
+ */
