9c070dd18d7e3d24351c91579b9802f808a7ffd3
Jo Wagner <jowagner@limago.de>
2024-08-13 14:16:36 +0200

    initial commit

--- Tag2_03Command/src/history/CommandHistory.java
+++ Tag2_03Command/src/history/CommandHistory.java
@@ -2,8 +2,13 @@ package history;
 
 import command.Command;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class CommandHistory {
 
+    private final Deque<Command> undoStack = new ArrayDeque<>();
+    private final Deque<Command> redoStack = new ArrayDeque<>();
     public void add(Command command) {
         if(command.isQuery()) return;
         // Code für Command
